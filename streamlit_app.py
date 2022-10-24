d97b46afcadfc28d34c22afbff5dbbab42a864dd
GitChris731 <116513859+GitChris731@users.noreply.github.com>
2022-10-24 19:18:38 +0100

    Update streamlit_app.py
    removed json line and separated based URL from fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) - removed as just writes the data to the screen
 
 # write your own comment -what does the next line do? normalises the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
